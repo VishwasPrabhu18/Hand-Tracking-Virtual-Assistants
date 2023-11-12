ffa0ceb2a2505523c395652a7322d13a880d7761
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2023-11-12 20:10:06 +0530

    all method including mainMethod also completed

--- main.py
+++ main.py
@@ -53,6 +53,60 @@ class handDetector():
                         cv2.circle(img, (bBox[0], bBox[1]), (bBox[2]+20, bBox[3]+20), (0, 255, 0))
         
         return self.lmList, bBox
+    
+    def fingerUp(self):
+        fingers = []
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0]-1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+            
+        for i in range(1, 5):
+            if self.lmList[self.tipIds[i]][2] < self.lmList[self.tipIds[i]-2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        
+        return fingers
+    
+    
+    def findDistance(self, p1, p2, img, draw=True):
+        x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
+        x2, y2 = self.lmList[p2][1], self.lmList[p2][2]
+        cx, cy = (x1 + x2)//2, (y1 + y2)//2
+        
+        if draw:
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            
+            length = math.hypot(x2-x1, y2-y1)
+            
+        return length, img, [x1, y1, x2, y2, cx, cy]                
 
 def mainMethod():
-    print("Hello, I'm Your Virtual Assistant!")
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) !=0:
+            print(lmList[4])
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if _name_ == "__main__":
+    mainMethod()
